d7b305651f9b16ed7a19c9127538d8568e5a2d91
Barto <juanmartin.cerruti@gmail.com>
2021-05-10 21:13:25 -0300

    docs

---
+++ docs/precache-manifest.ec726bf62f2c3737b1cd5cab188bff85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da245e2491f1583f2780ba4b863ac171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94b8e4cdcb9ed8ab8329",
+    "url": "/static/css/3.ec7d2d3f.chunk.css"
+  },
+  {
+    "revision": "a3c82c8b51d7c79287a7",
+    "url": "/static/css/4.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "10bef359c808de6d5a53",
+    "url": "/static/css/main.21f6a90e.chunk.css"
+  },
+  {
+    "revision": "690e26a44cfd21d3bd56",
+    "url": "/static/js/2.62b5e674.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.62b5e674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b8e4cdcb9ed8ab8329",
+    "url": "/static/js/3.36bc8f2c.chunk.js"
+  },
+  {
+    "revision": "a3c82c8b51d7c79287a7",
+    "url": "/static/js/4.c3b8b8de.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/static/js/4.c3b8b8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bef359c808de6d5a53",
+    "url": "/static/js/main.e5823cea.chunk.js"
+  },
+  {
+    "revision": "482c23cc44c576c2f077",
+    "url": "/static/js/runtime-main.ba359715.js"
+  }
+]);
