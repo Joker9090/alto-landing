5e5d267d5477fa52b55cb5a7ba41ed8c34684f34
Barto <juanmartin.cerruti@gmail.com>
2021-05-10 21:21:00 -0300

    Updates

---
+++ precache-manifest.3b135f8b189e1014e4cb5582d2a67872.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e04518ead2e34cf3020ef447c0b5cd4",
+    "url": "/alto-landing/index.html"
+  },
+  {
+    "revision": "4e2be16495b1b18a4d39",
+    "url": "/alto-landing/static/css/3.ec7d2d3f.chunk.css"
+  },
+  {
+    "revision": "a3c82c8b51d7c79287a7",
+    "url": "/alto-landing/static/css/4.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "019603a11e194eeb6c07",
+    "url": "/alto-landing/static/css/main.21f6a90e.chunk.css"
+  },
+  {
+    "revision": "690e26a44cfd21d3bd56",
+    "url": "/alto-landing/static/js/2.62b5e674.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/alto-landing/static/js/2.62b5e674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2be16495b1b18a4d39",
+    "url": "/alto-landing/static/js/3.5e46f763.chunk.js"
+  },
+  {
+    "revision": "a3c82c8b51d7c79287a7",
+    "url": "/alto-landing/static/js/4.c3b8b8de.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/alto-landing/static/js/4.c3b8b8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019603a11e194eeb6c07",
+    "url": "/alto-landing/static/js/main.5486f558.chunk.js"
+  },
+  {
+    "revision": "01f5174fee4c52e7d983",
+    "url": "/alto-landing/static/js/runtime-main.41e443f1.js"
+  }
+]);
